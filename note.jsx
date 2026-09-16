2d9cf285ee5f5a33de2fe50affcb3a8123d72b58
Alok Mishra <legendmighty16@gmail.com>
2026-09-17 03:37:04 +0530

    setup project files - part 1

---
+++ note.jsx
@@ -0,0 +1,7 @@
+// means git me ham ek bar folder push krne ke bad dubara dusre folder nahe kr skte sirf file he kr skte hain.
+// www.topal.com me gitignore.genrator file download krne ke leye use hota hai.
+
+// file folder me create krne ka shortcut tarika
+// => cd src se us folder me jao src folder name hai 
+// => ls ab lekho list create krne ke leye 
+// => touch app.js constants.js  now ab touch se create hoga file or app.js ye sb file name hain
